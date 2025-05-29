ca31de3f4cd6641021e04f5735ee5c3fbd438e0a
Julien Nioche <julien.nioche@digital.justice.gov.uk>
2025-05-29 13:37:12 +0100

    Generate csvs for accounts as well as namespaces
    Signed-off-by: Julien Nioche <julien.nioche@digital.justice.gov.uk>

--- coat-reports/README.md
+++ coat-reports/README.md
@@ -1,11 +1,17 @@
 
-docker run -it -v /Users/julien.nioche/data/curs:/curs -v /Users/julien.nioche/data/output:/output  apache/spark:3.5.5-java17 \
- /opt/spark/bin/spark-shell -i output/script.scala
+Apache Spark job to generate monthly csv reports of costs per namespace and account
+
+`mvn clean package`
+
+Expects the input to be CUR v2 files at Parquet format in a directory `BILLING_PERIOD=$billingPeriod`.
+Generates 2 CSV files with the costs aggregated per product.
+
+cp target/coat-reports-1.0.jar ../../output
 
 docker run -it -v /Users/julien.nioche/data/curs:/curs -v /Users/julien.nioche/data/output:/output  apache/spark:3.5.5-java17 \
 /opt/spark/bin/spark-submit  \
 --class ProcessParquetApp \
 --master 'local[*]' \
 /output/coat-reports-1.0.jar \
-/curs /output/test
+/curs /output 2025-04
 
--- coat-reports/src/main/java/ProcessParquetApp.java
+++ coat-reports/src/main/java/ProcessParquetApp.java
@@ -6,15 +6,22 @@ import static org.apache.spark.sql.functions.*;
 public class ProcessParquetApp {
 
     public static void main(String[] args) {
-        if (args.length < 2) {
-            System.err.println("Usage: ProcessParquetApp <inputPath> <outputPath>");
+        if (args.length < 3) {
+            System.err.println("Usage: ProcessParquetApp <inputPath> <outputPath> <billing_period> [byAccount]");
             System.exit(1);
         }
 
         String inputPath = args[0];
         String outputPath = args[1];
+        String billingPeriod = args[2];
 
-        SparkSession spark = SparkSession.builder()
+        // TODO check format YYYY-MM
+
+        outputPath+="/"+billingPeriod+"/";
+
+        inputPath += "/BILLING_PERIOD="+billingPeriod;
+
+        SparkSession spark = SparkSession.builder().config("mapreduce.fileoutputcommitter.marksuccessfuljobs", "false")
                 .appName("ProcessParquetApp")
                 .master("local[*]")
                 .getOrCreate();
@@ -22,33 +29,56 @@ public class ProcessParquetApp {
         // Read the input Parquet file
         Dataset<Row> parquetFileDF = spark.read().parquet(inputPath);
 
+            // Select and filter the relevant columns
+            Dataset<Row> selectedDF = parquetFileDF.select(
+                    col("line_item_usage_account_id").alias("account_id"),
+                    col("product").getItem("product_name").alias("product_name"),
+                    col("line_item_unblended_cost").alias("cost")
+            );
+
+            // Group by and aggregate
+            Dataset<Row> groupedDF = selectedDF.groupBy("account_id", "product_name")
+                    .agg(sum("cost").alias("total_unblended_cost")).withColumn("total_unblended_cost", round(col("total_unblended_cost"), 2));
+
+            // Sort the output
+            Dataset<Row> sortedDF = groupedDF.orderBy(
+                    asc("account_id"),
+                    desc("total_unblended_cost")
+            );
+
+            // Write the result to a single CSV file
+            sortedDF.coalesce(1)
+                    .write()
+                    .option("header", "true")
+                    .mode(SaveMode.Overwrite)
+                    .csv(outputPath+"costs_by_account_id");
+
         // Select and filter the relevant columns
-        Dataset<Row> selectedDF = parquetFileDF.select(
+        Dataset<Row> selectedDF2 = parquetFileDF.select(
                 col("resource_tags").getItem("user_namespace").alias("user_namespace"),
                 col("product").getItem("product_name").alias("product_name"),
                 col("line_item_unblended_cost").alias("cost")
         ).filter(
                 col("user_namespace").isNotNull()
                         .and(length(col("user_namespace")).gt(0))
-                        .and(col("cost").gt(0))
         );
 
         // Group by and aggregate
-        Dataset<Row> groupedDF = selectedDF.groupBy("user_namespace", "product_name")
+        Dataset<Row> groupedDF2 = selectedDF2.groupBy("user_namespace", "product_name")
                 .agg(sum("cost").alias("total_unblended_cost")).withColumn("total_unblended_cost", round(col("total_unblended_cost"), 2));;
 
         // Sort the output
-        Dataset<Row> sortedDF = groupedDF.orderBy(
+        Dataset<Row> sortedDF2 = groupedDF2.orderBy(
                 asc("user_namespace"),
                 desc("total_unblended_cost")
         );
 
-        // Write the result to a single CSV file
-        sortedDF.coalesce(1)
-                .write()
-                .option("header", "true")
-                .mode(SaveMode.Overwrite)
-                .csv(outputPath);
+            // Write the result to a single CSV file
+            sortedDF2.coalesce(1)
+                    .write()
+                    .option("header", "true")
+                    .mode(SaveMode.Overwrite)
+                    .csv(outputPath+"costs_by_namespace");
 
         spark.stop();
     }
