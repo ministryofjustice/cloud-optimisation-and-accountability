82795e128c32627793cf8e36378eacb7c117897c
Ratna Kurapati <ratna.kurapati@justice.gov.uk>
2025-08-04 10:20:33 +0100

    guthub modules

--- terraform/github/repositories/orgs.tf
+++ terraform/github/repositories/orgs.tf
@@ -1,5 +1,5 @@
-# module "ministryofjustice" {
-#   source = "./ministryofjustice"
+module "ministryofjustice" {
+  source = "./ministryofjustice"
 
-#   cloud_optimisation_and_accountability_team_id = data.github_team.cloud_optimisation_and_accountability.id
-# }
+  cloud_optimisation_and_accountability_team_id = data.github_team.cloud_optimisation_and_accountability.id
+}
