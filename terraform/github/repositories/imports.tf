cde94572181a7d71567d2476cc780cea15f13697
Ratna Kurapati <ratna.kurapati@justice.gov.uk>
2025-08-04 14:58:45 +0100

    github module imports

---
+++ terraform/github/repositories/imports.tf
@@ -0,0 +1,35 @@
+# importing github_branch_protection
+import {
+  to = module.ministryofjustice.module.cloud-optimisation-and-accountability.github_branch_protection.default
+  id = "BPR_kwDOOWCmUs4Dq9DA"
+}
+
+# importing github_repository
+import {
+  to = module.ministryofjustice.module.cloud-optimisation-and-accountability.github_repository.default
+  id = "cloud-optimisation-and-accountability"
+}
+
+# importing github_team_repository
+import {
+  to = module.ministryofjustice.module.cloud-optimisation-and-accountability.github_team_repository.admin["12623342"]
+  id = "12623342:cloud-optimisation-and-accountability"
+}
+
+# importing github_branch_protection
+import {
+  to = module.ministryofjustice.module.coat-cur-data-pipeline.github_branch_protection.default
+  id = "BPR_kwDOPEW5ds4D2jgG"
+}
+
+# importing github_repository
+import {
+  to = module.ministryofjustice.module.coat-cur-data-pipeline.github_repository.default
+  id = "coat-cur-data-pipeline"
+}
+
+# importing github_team_repository
+import {
+  to = module.ministryofjustice.module.coat-cur-data-pipeline.github_team_repository.admin["12623342"]
+  id = "12623342:coat-cur-data-pipeline"
+}
