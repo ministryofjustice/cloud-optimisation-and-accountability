ce3fe0ef863f136a170ecd7e8062fc140248b5c0
connormaglynn <connor.glynn@digital.justice.gov.uk>
2025-08-05 23:28:42 +0100

    ✨ Add Explicit Provider Versions

--- terraform/github/repositories/move.tf
+++ terraform/github/repositories/move.tf
@@ -4,32 +4,32 @@ moved {
   to   = module.ministryofjustice-new.module.cloud-optimisation-and-accountability-new.github_branch_protection.default
 }
 
-# # moved github_repository
-# moved {
-#   from = module.ministryofjustice.module.cloud-optimisation-and-accountability.github_repository.default
-#   to   = module.ministryofjustice-new.module.cloud-optimisation-and-accountability-new.github_repository.default
-# }
+# moved github_repository
+moved {
+  from = module.ministryofjustice.module.cloud-optimisation-and-accountability.github_repository.default
+  to   = module.ministryofjustice-new.module.cloud-optimisation-and-accountability-new.github_repository.default
+}
 
-# # moved github_team_repository
-# moved {
-#   from = module.ministryofjustice.module.cloud-optimisation-and-accountability.github_team_repository.admin["12623342"]
-#   to   = module.ministryofjustice-new.module.cloud-optimisation-and-accountability-new.github_team_repository.admin["12623342"]
-# }
+# moved github_team_repository
+moved {
+  from = module.ministryofjustice.module.cloud-optimisation-and-accountability.github_team_repository.admin["12623342"]
+  to   = module.ministryofjustice-new.module.cloud-optimisation-and-accountability-new.github_team_repository.admin["12623342"]
+}
 
-# # moved github_branch_protection
-# moved {
-#   from = module.ministryofjustice.module.coat-cur-data-pipeline.github_branch_protection.default
-#   to   = module.ministryofjustice-new.module.coat-cur-data-pipeline-new.github_branch_protection.default
-# }
+# moved github_branch_protection
+moved {
+  from = module.ministryofjustice.module.coat-cur-data-pipeline.github_branch_protection.default
+  to   = module.ministryofjustice-new.module.coat-cur-data-pipeline-new.github_branch_protection.default
+}
 
-# # moved github_repository
-# moved {
-#   from = module.ministryofjustice.module.coat-cur-data-pipeline.github_repository.default
-#   to   = module.ministryofjustice-new.module.coat-cur-data-pipeline-new.github_repository.default
-# }
+# moved github_repository
+moved {
+  from = module.ministryofjustice.module.coat-cur-data-pipeline.github_repository.default
+  to   = module.ministryofjustice-new.module.coat-cur-data-pipeline-new.github_repository.default
+}
 
-# # moved github_team_repository
-# moved {
-#   from = module.ministryofjustice.module.coat-cur-data-pipeline.github_team_repository.admin["12623342"]
-#   to   = module.ministryofjustice-new.module.coat-cur-data-pipeline-new.github_team_repository.admin["12623342"]
-# }
+# moved github_team_repository
+moved {
+  from = module.ministryofjustice.module.coat-cur-data-pipeline.github_team_repository.admin["12623342"]
+  to   = module.ministryofjustice-new.module.coat-cur-data-pipeline-new.github_team_repository.admin["12623342"]
+}
