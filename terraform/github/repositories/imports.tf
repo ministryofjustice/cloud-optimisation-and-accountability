83d746e4bd4bf8a567a22bfbedd414199f3310e2
Ratna Kurapati <ratna.kurapati@justice.gov.uk>
2025-08-05 10:59:15 +0100

    github repo module reference update

--- terraform/github/repositories/imports.tf
+++ terraform/github/repositories/imports.tf
@@ -34,40 +34,39 @@ import {
   id = "12623342:coat-cur-data-pipeline"
 }
 
-
 # moved github_branch_protection
 moved {
   from = module.ministryofjustice.module.cloud-optimisation-and-accountability.github_branch_protection.default
-  to   = module.ministryofjustice.module.cloud-optimisation-and-accountability.github_branch_protection.default
+  to   = module.github-repository.module.cloud-optimisation-and-accountability.github_branch_protection.default
 }
 
 # moved github_repository
 moved {
   from = module.ministryofjustice.module.cloud-optimisation-and-accountability.github_repository.default
-  to   = module.ministryofjustice.module.cloud-optimisation-and-accountability.github_repository.default
+  to   = module.github-repository.module.cloud-optimisation-and-accountability.github_repository.default
 }
 
 # moved github_team_repository
 moved {
   from = module.ministryofjustice.module.cloud-optimisation-and-accountability.github_team_repository.admin["12623342"]
-  to   = module.ministryofjustice.module.cloud-optimisation-and-accountability.github_team_repository.admin["12623342"]
+  to   = module.github-repository.module.cloud-optimisation-and-accountability.github_team_repository.admin["12623342"]
 }
 
 # moved github_branch_protection
 moved {
   from = module.ministryofjustice.module.coat-cur-data-pipeline.github_branch_protection.default
-  to   = module.ministryofjustice.module.coat-cur-data-pipeline.github_branch_protection.default
+  to   = module.github-repository.module.coat-cur-data-pipeline.github_branch_protection.default
 }
 
 # moved github_repository
 moved {
   from = module.ministryofjustice.module.coat-cur-data-pipeline.github_repository.default
-  to   = module.ministryofjustice.module.coat-cur-data-pipeline.github_repository.default
+  to   = module.github-repository.module.coat-cur-data-pipeline.github_repository.default
 }
 
 # moved github_team_repository
 moved {
   from = module.ministryofjustice.module.coat-cur-data-pipeline.github_team_repository.admin["12623342"]
-  to   = module.ministryofjustice.module.coat-cur-data-pipeline.github_team_repository.admin["12623342"]
+  to   = module.github-repository.module.coat-cur-data-pipeline.github_team_repository.admin["12623342"]
 }
 
