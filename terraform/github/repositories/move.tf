12b30167eb1e1ae32956a7e67197479b0de08029
Ratna Kurapati <ratna.kurapati@justice.gov.uk>
2025-08-05 11:02:37 +0100

    github repo module reference update

--- terraform/github/repositories/move.tf
+++ terraform/github/repositories/move.tf
@@ -1,39 +1,3 @@
-# importing github_branch_protection
-import {
-  to = module.ministryofjustice.module.cloud-optimisation-and-accountability.github_branch_protection.default
-  id = "cloud-optimisation-and-accountability:main"
-}
-
-# importing github_repository
-import {
-  to = module.ministryofjustice.module.cloud-optimisation-and-accountability.github_repository.default
-  id = "cloud-optimisation-and-accountability"
-}
-
-# importing github_team_repository
-import {
-  to = module.ministryofjustice.module.cloud-optimisation-and-accountability.github_team_repository.admin["12623342"]
-  id = "12623342:cloud-optimisation-and-accountability"
-}
-
-# importing github_branch_protection
-import {
-  to = module.ministryofjustice.module.coat-cur-data-pipeline.github_branch_protection.default
-  id = "coat-cur-data-pipeline:main"
-}
-
-# importing github_repository
-import {
-  to = module.ministryofjustice.module.coat-cur-data-pipeline.github_repository.default
-  id = "coat-cur-data-pipeline"
-}
-
-# importing github_team_repository
-import {
-  to = module.ministryofjustice.module.coat-cur-data-pipeline.github_team_repository.admin["12623342"]
-  id = "12623342:coat-cur-data-pipeline"
-}
-
 # moved github_branch_protection
 moved {
   from = module.ministryofjustice.module.cloud-optimisation-and-accountability.github_branch_protection.default
